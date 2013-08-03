6c7536849b3da86652632cb7f5910c41d9306656
Volker Knollmann <>
2013-08-02 22:36:25 -0400

    Initial commit of an empty project for an JDBC overlay for easy database access

---
+++ src/org/nodomain/volkerk/JDBCOverlayLib/JDBC_GenericDB.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.nodomain.volkerk.JDBCOverlayLib;
+
+/**
+ *
+ * @author volker
+ */
+abstract public class JDBC_GenericDB {
+    
+}
